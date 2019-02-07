d3a2ce06a52625cb54d8d07e94fa673ae297d06c
xMing <526817087@qq.com>
2019-02-07 14:55:58 +0800

    Update UISender.java

--- src/yxmingy/yupi/UISender.java
+++ src/yxmingy/yupi/UISender.java
@@ -2,13 +2,13 @@ package yxmingy.yupi;
 
 import cn.nukkit.network.protocol.ModalFormRequestPacket;
 
-import java.util.HashMap;
+import java.util.Map;
 import com.google.gson.GsonBuilder;
 
 import cn.nukkit.Player;
 
 public class UISender {
-	public static void sendUI(int formid, HashMap<String, String> formdata,Player player)
+	public static void sendUI(int formid, Map<String, Object> formdata,Player player)
 	{
 		ModalFormRequestPacket ui = new ModalFormRequestPacket();
     ui.formId = formid;
