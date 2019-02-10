5a04efbc066a434b548a57c595870f740a3f3614
YxMingY <526817087@qq.com>
2019-02-10 16:47:04 +0800

    #3 添加工具方法buildId()

--- yxmingy/yupi/Utils.java
+++ yxmingy/yupi/Utils.java
@@ -5,6 +5,15 @@ import cn.nukkit.event.*;
 
 
 public class Utils extends PluginBase implements Listener{
+	public static int buildId(String data){
+	  int i,progress = 1,id = 0,length = data.length();
+	  char[] cdatas = data.toCharArray();
+	  for(i=0;i<length;i++){
+	    id += ((int)(cdatas[i]))*progress;
+	    progress *= 10;
+	  }
+	  return id;
+	}
 	public void onEnable() {
 		getLogger().notice("YUPI is Enabled! Author: xMing.");
 		getServer().getPluginManager().registerEvents(this, this);
