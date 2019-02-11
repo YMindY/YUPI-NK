8ddf9d2345dbf17dbc92cb331019701fb19869dc
YxMingY <526817087@qq.com>
2019-02-11 18:21:15 +0800

    #6 添加花里胡哨的UI(costom_form)

--- yxmingy/yupi/ui/GarishForm.java
+++ yxmingy/yupi/ui/GarishForm.java
@@ -4,7 +4,7 @@ import java.util.*;
 
 public class GarishForm extends UIBase{
 
-  private ArrayList<Map<String, String>> content;
+  private ArrayList<Map<String, Object>> content;
   
   public GarishForm(String title)
   {
@@ -13,20 +13,55 @@ public class GarishForm extends UIBase{
   }
   public void addLabel(String text)
   {
-    Map<String,String> content = new LinkedHashMap<>();
+    Map<String,Object> content = new LinkedHashMap<>();
     content.put("type","label");
+    content.put("text",text);
+    addContent(content);
+  }
+  public void addToggle(String text)
+  {
+    Map<String,Object> content = new LinkedHashMap<>();
+    content.put("type","toggle");
     content.put("text",text);
     addContent(content);
   }
   public void addToggle(String text,boolean _default)
   {
-    Map<String,String> content = new LinkedHashMap<>();
+    Map<String,Object> content = new LinkedHashMap<>();
     content.put("type","toggle");
     content.put("text",text);
-    content.put("default",_default ? "true" : "false");
+    content.put("default",_default);
     addContent(content);
   }
-  public void addContent(Map<String,String> content)
+  public void addSlider(String text,int min,int max,int step)
+  {
+    Map<String,Object> content = new LinkedHashMap<>();
+    content.put("type","slider");
+    content.put("text",text);
+    content.put("min",min);
+    content.put("max",max);
+    content.put("step",step);
+    addContent(content);
+  }
+  public void addDropdown(String text,String[] options)
+  {
+    Map<String,Object> content = new LinkedHashMap<>();
+    content.put("type","dropdown");
+    content.put("text",text);
+    content.put("options",options);
+    content.put("default",null);
+    addContent(content);
+  }
+  public void addInput(String text,String placeholder)
+  {
+    Map<String,Object> content = new LinkedHashMap<>();
+    content.put("type","input");
+    content.put("text",text);
+    content.put("placeholder",placeholder);
+    content.put("default",null);
+    addContent(content);
+  }
+  public void addContent(Map<String,Object> content)
   {
     this.content.add(content);
   }
