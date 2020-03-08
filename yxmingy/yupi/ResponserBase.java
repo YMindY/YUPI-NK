1b277c30baf1870fe099a3dc77841de3a3560f3a
xMing <526817087@qq.com>
2020-03-08 20:26:17 +0800

    Update ResponserBase.java

--- yxmingy/yupi/ResponserBase.java
+++ yxmingy/yupi/ResponserBase.java
@@ -12,16 +12,16 @@ abstract public class ResponserBase implements Listener{
 	abstract protected void response(int id,String index,Player player);
 	
 	@EventHandler
-  public void onServerReceivePacket(DataPacketReceiveEvent event)
-  {
-    String index;
+	public void onServerReceivePacket(DataPacketReceiveEvent event)
+	{
+		String index;
 		Player player = event.getPlayer();
 		ModalFormResponsePacket ui;
 		if(!(event.getPacket() instanceof ModalFormResponsePacket)) return;
 		//if(!(player instanceof Player)) return; /* wtfk? 我傻了? */
-    ui = (ModalFormResponsePacket)event.getPacket();///???
-    index = ui.data.trim();
-    //if(index.equals("null")) return;
-    response(ui.formId,index,player);
-  }
+		ui = (ModalFormResponsePacket)event.getPacket();///???
+		index = ui.data.trim();
+		//if(index.equals("null")) return;
+		response(ui.formId,index,player);
+	}
 }
